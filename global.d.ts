a53e8dda08c82170adf6428e7b4bf1312cca1022
ClaraCrazy <clara@cynthialabs.net>
2024-05-21 21:28:23 +0200

    WHY TF does this work...

---
+++ global.d.ts
@@ -0,0 +1,62 @@
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+declare module "*.html" {
+  const content: string;
+  export default content;
+}
+
+declare const IS_DEV: boolean;
+declare const DEFAULT_LANG: Record<string, string> | undefined;
+declare const DEV_LANG: Record<string, Record<string, string>> | undefined;
+
+// Buffer types coming soon:tm:
+declare const Buffer: any;
+
+interface Window {
+  nx?: {
+    readonly semantic: Record<
+      string,
+      Record<"dark" | "darker" | "light" | "amoled", string>
+    >;
+    searchSemantic: (query: string) => any;
+    findSemantic: (hex: string) => any;
+    p: {
+      wipe: () => void;
+      snipe: (
+        prop: string,
+        parent: any,
+        callback?: (args: any[], ret: any) => any,
+        oneTime?: boolean,
+      ) => void;
+      props: {
+        collect: (
+          key: string,
+          prop: string,
+          parent: any,
+          parser?: (obj: any) => any,
+        ) => void;
+        redeem: (key: string, save?: boolean) => object | undefined;
+      };
+    };
+  };
+
+  __vendetta_loader: {
+    features?: {
+      themes?: { prop: string };
+      syscolors?: { prop: string };
+    };
+  };
+  __vendetta_theme: Theme | undefined;
+
+  TPfirstLoad: boolean | undefined;
+}
